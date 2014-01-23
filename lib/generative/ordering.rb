fd0eb486853bc7ff078ab414618e98be81a0006f
Justin Campbell <justin@justincampbell.me>
2014-01-23 17:26:30 -0500

    Only multiply specs tagged with :generative
    Thanks @nicholaides

--- lib/generative/ordering.rb
+++ lib/generative/ordering.rb
@@ -1,12 +1,17 @@
 Generative::ORDERING = ->(examples) {
+  generative, regular = examples.partition { |example|
+    example.metadata[:generative]
+  }
+
   number_of_examples = ENV.fetch('GENERATIVE_COUNT', '1').to_i
-  examples * number_of_examples
+
+  regular + generative * number_of_examples
 }
 
 RSpec.configure do |rspec|
   case RSpec::Core::Version::STRING
   when /^2/
-    rspec.order_examples(&Generative::ORDERING) if ENV['GENERATIVE_COUNT']
+    rspec.order_examples(&Generative::ORDERING)
   when /^3/
     rspec.register_ordering(:generative, &Generative::ORDERING)
   else
